99e6fec88c3543785e29fe89cf345520196e277e
zhoiton <k_volodiev@abv.bg>
2018-04-12 16:17:38 +0300

    added tests for the proposition class

---
+++ ALE_UnitTest/UnitTestProposition.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ALE1_test;
+
+namespace ALE_UnitTest
+{
+    [TestClass]
+    public class UnitTestProposition
+    {
+        [TestMethod]
+        public void doesItConsumePropsCorrectlyTest()
+        {
+            Proposition proposition = new Proposition("|(=(>(d,B),>(s,B)),=(>(c,B),>(s,g)))");
+            string expected_proposition = "|(=(>(d,B),>(s,B)),=(>(c,B),>(s,g)))";
+            Assert.AreEqual(expected_proposition, proposition.getPropositionASCIIFormat());
+        }
+        [TestMethod]
+        public void doesItCalculatePropsCorrectlyTest()
+        {
+            Proposition proposition = new Proposition("|(=(>(1,1),>(1,1)),=(>(1,1),>(1,1)))");
+            bool expected_result = true;
+            Assert.AreEqual(expected_result, proposition.calculate());
+        }
+    }
+}
