965dfc4a2034b9feeabdd67500df7ea5a29bda80
zhoiton <k_volodiev@abv.bg>
2018-04-12 14:32:37 +0300

    added testes for the normalizator class

---
+++ ALE_UnitTest/UnitTestNormalizer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ALE1_test;
+
+namespace ALE_UnitTest
+{
+    [TestClass]
+    public class UnitTestNormalizer
+    {
+        string[,] truth_table = { { "a", "B", ">(a,B)" }, { "1", "1", "1" }, { "1", "0", "0" }, { "0", "1", "1" }, { "0", "0", "1" } };
+        string[,] simplified_table = { { "a", "B", ">(a,B)" }, { "1", "0", "0" }, { "*", "1", "1" }, { "0", "*", "1" } };
+            
+        [TestMethod]
+        public void TestDNF()
+        {
+            Normalizator normalizor = new Normalizator(truth_table,simplified_table);
+            string expected_result = "|(|(&(a,B),&(~(a),B)),&(~(a),~(B)))";
+            string resut = normalizor.getDNFFromTruthTable();
+            Assert.AreEqual(expected_result, resut);
+        }
+        [TestMethod]
+        public void TestSimplifiedDNF()
+        {
+            Normalizator normalizor = new Normalizator(truth_table, simplified_table);
+            string expected_result = "|(B,~(a))";
+            string resut = normalizor.getDNFFromSimplifiedTruthTable();
+            Assert.AreEqual(expected_result, resut);
+        }
+    }
+}
