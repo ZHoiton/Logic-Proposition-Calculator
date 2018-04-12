03d4ba00fa06db47addf4b55c60a003cccb6e11d
zhoiton <k_volodiev@abv.bg>
2018-04-12 16:21:52 +0300

    added tests fot the predicate class

---
+++ ALE_UnitTest/UnitTestPredicate.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ALE1_test;
+
+namespace ALE_UnitTest
+{
+    [TestClass]
+    public class UnitTestPredicate
+    {
+        [TestMethod]
+        public void consumeTest()
+        {
+            Predicate predicate = new Predicate("a");
+            Assert.AreEqual("a", predicate.getPredicateASCIIFormat());
+        }
+        [TestMethod]
+        public void consumeNOTTest()
+        {
+            Predicate predicate = new Predicate("~(a)");
+            Assert.AreEqual("~(a)", predicate.getPredicateASCIIFormat());
+        }
+        [TestMethod]
+        public void calculateTest()
+        {
+            Predicate predicate = new Predicate("1");
+            Assert.AreEqual(true, predicate.getValue());
+        }
+        [TestMethod]
+        public void calculateNOTTest()
+        {
+            Predicate predicate = new Predicate("~(1)");
+            Assert.AreEqual(false, predicate.getValue());
+        }
+        [TestMethod]
+        public void calculateNOTSecondTest()
+        {
+            Predicate predicate = new Predicate("~(0)");
+            Assert.AreEqual(true, predicate.getValue());
+        }
+    }
+}
